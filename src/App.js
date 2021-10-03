75e3d34efe769a5e21ac563b810caa9bf0aa8208
oaanees <81834401+oaanees@users.noreply.github.com>
2021-10-02 23:57:39 -0500

    commit #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import { BrowserRouter, Route, Switch } from "react-router-dom"; //So that we can go to different pages
+import LoginForm from './Components/LoginForm';
+// import Home from './Components/Home';
+// <a href = ".Components/ScheduleInput">Plan your schedule</a>
+
 
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if(details.email == adminUser.email && details.password == adminUser.password){
+      console.log("Logged in");
+
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    }
+      
+    else {
+      console.log("Details do not match!");
+      setError("Details do not match!");
+      
+    }
+  }
+
+  const Logout = () => {
+    setUser({name: "", email: ""});
+  }
+
   return (
+    // Create Different Route
+    // <BrowserRouter>
+    //   <Switch>
+    //     <Route component={Home} path='/home' />
+    //   </Switch>
+    // </BrowserRouter>
+
+    //Original one
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
-
 export default App;
